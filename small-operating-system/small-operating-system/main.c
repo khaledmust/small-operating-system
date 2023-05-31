fd0eb37a2f3ad2db40d4bcd404cabf3a44a7d239
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-05-31 22:37:59 +0300

    Configure project with Microchip IDE

---
+++ small-operating-system/small-operating-system/main.c
@@ -0,0 +1,18 @@
+/*
+ * small-operating-system.c
+ *
+ * Created: 2023-05-31 10:37:00 PM
+ * Author : khale
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
