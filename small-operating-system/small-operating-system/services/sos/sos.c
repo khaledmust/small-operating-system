3645d82601d11506d93a6b73868af1bc3bc63084
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-06-01 02:35:53 +0300

    Implement the Deinit function.

--- small-operating-system/small-operating-system/services/sos/sos.c
+++ small-operating-system/small-operating-system/services/sos/sos.c
@@ -1,11 +1,15 @@
 #include "sos.h"
 #include <stdlib.h>
 
+/* Initialize the DB global pointer to NULL. */
 st_SOS_task_property_t* (*g_ptr_SOS_db)[5] = NULL;
 
 /**
- * @brief Creates the DB instance and assigns the address of this DB to the global pointer.
- *
+ * @brief Creates the DB instance and assigns the address of this DB to the global DB pointer.
+ *  - This is done using the malloc() function where we allocate the DB on the head in accordance to the number of tasks
+ *  defined in the sos.h file.
+ *  - Then the address of the allocated DB is assigned to the DB global address.
+ *  - A nullification to all the elements of the array to prevent segmentation fault.
  * @return en_SOS_SYSTEM_STATUS_t
  */
 en_SOS_SYSTEM_STATUS_t SOS_init(void) {
@@ -23,6 +27,24 @@ en_SOS_SYSTEM_STATUS_t SOS_init(void) {
         for (i = 0; i < NUM_OF_TASKS; i++) {
             (*g_ptr_SOS_db)[i] = NULL;
         }
+    }
+    return SOS_STATUS_SUCCESS;
+}
+
+/**
+ * @brief De-allocates the created DB instance and pointing the global DB pointer to NULL.
+ * - This is done using the free() function.
+ * @return en_SOS_SYSTEM_STATUS_t
+ */
+en_SOS_SYSTEM_STATUS_t SOS_deinit(void) {
+    if (g_ptr_SOS_db == NULL) {
+        return SOS_STATUS_INVALID_STATE;
+    } else {
+        /* Delete the DB from the heap. */
+        free(g_ptr_SOS_db);
+
+        /* Assign the address of the DB to NULL to prevent segmentation fault. */
+        g_ptr_SOS_db = NULL;
     }
     return SOS_STATUS_SUCCESS;
 }
